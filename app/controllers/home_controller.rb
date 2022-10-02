63a1548a98d3a98ae3c8f62f9328fd48ac595524
Isaac Pure <isaacpure@gmail.com>
2022-10-02 17:20:24 -0500

    making changes to try and trouble shoot errors

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,5 @@
 class HomeController < ApplicationController
   def index
-    total = Transaction.where(user_id: current_user)
-    @sum_trans = total.where(user_id: current_user).sum(:amount)
-    @current_balance = current_user.balance - @sum_trans
   end
 
   def about
